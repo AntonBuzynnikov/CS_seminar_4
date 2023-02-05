295f6cbacf38fed608f49227d9687e0248b24751
Buzynnikov <buzynnikov2111@gmail.com>
2023-02-05 19:40:42 +0700

    Complete Example040

---
+++ Example040/Program.cs
@@ -0,0 +1,25 @@
+﻿// 40. Задать массив из 8 целых элементов и вывести их на экран
+System.Console.Write("Введите длину массива: ");
+
+int length = Int32.Parse(System.Console.ReadLine());
+
+void CreateArray(int x)
+{
+    int[] array = new int[x];
+    System.Console.WriteLine("Вводите элементы массива"); 
+    for(int i = 0; i < x; i++)
+    {
+        array[i] = Int32.Parse(System.Console.ReadLine());
+        
+    }
+    System.Console.Write("Массив: ");
+    for(int i = 0; i < x; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+
+}
+
+CreateArray(length);
+
+
