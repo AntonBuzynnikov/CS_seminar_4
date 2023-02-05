dd5847bfbc023ad305eb779fd830f6345eb9933d
Buzynnikov <buzynnikov2111@gmail.com>
2023-02-05 20:00:15 +0700

    Complete Example041

---
+++ Example041/Program.cs
@@ -0,0 +1,22 @@
+﻿// 41. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+System.Console.Write("Введите длину массива: ");
+
+int length = Int32.Parse(System.Console.ReadLine());
+
+void CreateArray(int x)
+{
+    int[] array = new int[x];
+    Random N = new Random();
+    for(int i = 0; i < x; i++)
+    {
+        array[i] = N.Next(2);
+    }
+    System.Console.Write("Массив: ");
+    for(int i = 0; i < x; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+
+}
+
+CreateArray(length);
