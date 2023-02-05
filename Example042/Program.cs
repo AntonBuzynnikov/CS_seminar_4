7bcdc3daeddc612b779d79b2dd932140c41dd739
Buzynnikov <buzynnikov2111@gmail.com>
2023-02-05 20:17:28 +0700

    Complete Example042

---
+++ Example042/Program.cs
@@ -0,0 +1,32 @@
+﻿// 42. Задать массив из 12 элементов, заполненных числами из [-9, 9].
+// Найти сумму положительных/отрицательных элементов массива
+System.Console.Write("Введите длину массива: ");
+
+int length = Int32.Parse(System.Console.ReadLine());
+
+void CreateArray(int x)
+{
+    int[] array = new int[x];
+    Random N = new Random();
+    for(int i = 0; i < x; i++)
+    {
+        array[i] = N.Next(-9,9);
+    }
+    System.Console.Write("Массив: ");
+    for(int i = 0; i < x; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+    int sumPositive = 0;
+    int sumNegative = 0;   
+    for(int i = 0; i < x; i++)
+    {
+        if(array[i] > 0) sumPositive+=array[i];
+        if(array[i] < 0) sumNegative+=array[i];
+    }
+    System.Console.WriteLine($"Сумма положительных чисел: {sumPositive}");
+    System.Console.WriteLine($"Сумма отрицательных чисел: {Math.Abs(sumNegative)}");
+}
+
+CreateArray(length);
