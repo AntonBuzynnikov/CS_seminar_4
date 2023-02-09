79c50736d8fd42378711e115a2e925248be88478
Buzynnikov <buzynnikov2111@gmail.com>
2023-02-09 08:48:18 +0700

    Complete Example044

---
+++ Example044/Program.cs
@@ -0,0 +1,24 @@
+﻿//
+int length = Int32.Parse(Console.ReadLine());
+int[] array = new int[length];
+//System.Console.WriteLine("Вводите элементы массива"); 
+for(int i = 0; i < array.Length; i++)
+{
+    Random N = new Random();
+    array[i] = N.Next(-10,10);    
+}
+System.Console.Write("Массив: ");
+for(int i = 0; i < array.Length; i++)
+{
+    System.Console.Write($"{array[i]} ");
+}
+System.Console.WriteLine();
+//Линейный поиск
+int find = 5;
+int j = 0;
+while (j<array.Length && array[j]!= find) j++;
+if (j == array.Length)
+    System.Console.WriteLine("NO");
+else
+    System.Console.WriteLine($"Элемент {find} находится под индексом {j}");
+
